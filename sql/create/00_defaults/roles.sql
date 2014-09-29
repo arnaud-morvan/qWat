7321fa92195c9b33cdd8cbbb5ae4dd66410cba76
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2014-09-29 17:08:21 +0200

    Finalize roles credentials

--- sql/create/00_defaults/roles.sql
+++ sql/create/00_defaults/roles.sql
@@ -1,14 +1,14 @@
 
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
     GRANT SELECT ON TABLES TO qwat_ro;
-GRANT SELECT ON public.geometry_columns TO qwat_ro;
 
+GRANT ALL ON SCHEMA distribution TO qwat_ro;
 ALTER DEFAULT PRIVILEGES IN SCHEMA distribution
     GRANT SELECT ON TABLES TO qwat_ro;
 
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
     GRANT SELECT ON TABLES TO qwat_rw;
-GRANT SELECT ON public.geometry_columns TO qwat_rw;
 
+GRANT ALL ON SCHEMA distribution TO qwat_rw;
 ALTER DEFAULT PRIVILEGES IN SCHEMA distribution
     GRANT ALL ON TABLES TO qwat_rw;
